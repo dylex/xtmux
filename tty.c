196ceab7e48ec1594f8add2ec11d0c51d24f7e94
Dylan Simon <dylan@dylex.net>
2014-08-29 21:35:02 -0400

    Add cursor style cap to xtmux.terminfo, and don't suppress those above 4
    This may break backwards compatibility, but allows xtmux cursor style
    extensions within nested tmux.  Also add unrelated ech cap.

--- tty.c
+++ tty.c
@@ -555,7 +555,7 @@ tty_update_mode(struct tty *tty, int mode, struct screen *s)
 		else
 			tty_putcode(tty, TTYC_CIVIS);
 	}
-	if (tty->cstyle != s->cstyle && s->cstyle <= 4) {
+	if (tty->cstyle != s->cstyle) {
 		if (tty_term_has(tty->term, TTYC_SS)) {
 			if (s->cstyle == 0 &&
 			    tty_term_has(tty->term, TTYC_SE))
